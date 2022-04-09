7eee445f2c38be6c48fd77f35ff8d5dc1cd362a9
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-04-09 13:31:25 +0000

    added authentication roote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Acceuil, Home, Login, Rdv, Signup } from "./pages";
+import { Acceuil, Home, Login, Rdv, Signup, Historique, Question } from "./pages";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => (
@@ -11,6 +11,8 @@ const App = () => (
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/rdv" element={<Rdv />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/historique" element={<Historique />}></Route>
+        <Route path="/question" element={<Question />}></Route>
       </Routes>
     </Router>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,5 @@ export { default as Acceuil } from "./acceuil/Acceuil";
 export { default as Signup } from "./signup/Signup";
 export { default as Rdv } from "./rdv/Rdv";
 export { default as Home } from "./home/Home";
+export { default as Question } from "./question/Question";
+export { default as Historique } from "./historique/Historique";
