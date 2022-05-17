7281ea4436a6f8aa32acbc3b3fd38c65e3dea432
hamza <hitec.appdev@gmail.com>
2022-05-18 00:13:17 +0100

    integration of Web-To-Case and remaking the question  UI page

--- src/pages/question/Question.js
+++ src/pages/question/Question.js
@@ -1,22 +1,71 @@
 import React from "react";
 import { Navbar } from "../../components";
 import "./question.css";
+import AuthService from "../../services/auth.service";
 
-const Question = () => (
+const Question = () => {
+  const user = AuthService.getCurrentUser();
+
+  console.log(user.email);
+
+  return (
     <div className="container bg-2">
-        <Navbar active="question" status="authentified"></Navbar>
-        <div className="wrapper">
-            <div className="card">
-                <h1>Poser une question</h1>
-                <hr />
-                <form className="question-form">
-                    <input type="text" placeholder="question.." name="question" required />
+      <Navbar active="question" status="authentified"></Navbar>
+      <div className="wrapper">
+        <div className="card">
+          <h1>Poser une question</h1>
+          <hr />
 
-                    <input type="submit" value="Envoyer" />
-                </form>
-            </div>
+          <form
+            className="question-form"
+            action="https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8"
+            method="POST"
+          >
+            <input type="hidden" name="orgid" value="00D8d000004IxgI" />
+            <input type="hidden" name="retURL" value="http://www.google.com" />
+            <input
+              id="email"
+              value={user.email}
+              maxlength="80"
+              name="email"
+              size="20"
+              type="hidden"
+            />
+
+            <label for="subject">Subject</label>
+            <input
+              placeholder="Taper le sujet.."
+              id="subject"
+              maxlength="80"
+              name="subject"
+              size="20"
+              type="text"
+            />
+            <br />
+
+            <label for="description">Description</label>
+            <textarea
+              rows="4"
+              name="description"
+              placeholder="Taper une description.."
+            ></textarea>
+            <br />
+
+            <label for="priority">Priority</label>
+            <select id="priority" name="priority">
+              <option value="">--None--</option>
+              <option value="High">Elevée</option>
+              <option value="Medium">Moyen</option>
+              <option value="Low">Faible</option>
+            </select>
+            <br></br>
+
+            <input type="submit" name="submit" />
+          </form>
         </div>
+      </div>
     </div>
-);
+  );
+};
 
 export default Question;
